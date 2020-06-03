a7cac8b37337906cbc6dfff0ca20b17c21f1338a
kondoumh <kondoumh@gmail.com>
2020-06-04 05:54:12 +0900

    Add search box

--- src/components/ScrapboxPages.vue
+++ src/components/ScrapboxPages.vue
@@ -1,33 +1,47 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="pages"
-    :items-per-page="itemsPerPage"
-    :footer-props="{
-      'items-per-page-options': [5, 10, 15, 20, 25, 30]
-    }"
-    class="elevation-1"
-  >
-    <template v-slot:item.pin="{ item }">
-      {{ item.pin != 0 ? "&#128204;" : "" }}
-    </template>
-    <template v-slot:item.views="{ item }">
-      {{ item.views }}
-    </template>
-    <template v-slot:item.linked="{ item }">
-      {{ item.linked }}
-    </template>
-    <template v-slot:item.updated="{ item }">
-      {{ formatDate(item.updated) }}
-    </template>
-    <template v-slot:item.title="{ item }">
-      <a
-      :href="'https://scrapbox.io/'+ projectName + '/' + encodeTitle(item.title)"
-      target="_blank">{{ item.title }}</a>    </template>
-    <template v-slot:item.image="{ item }">
-      <img :src="item.image" style="width: auto; height: 25px">
-    </template>
-  </v-data-table>
+  <v-card>
+    <v-card-title>
+      {{ projectName }}
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :items="pages"
+      :search="search"
+      :items-per-page="itemsPerPage"
+      :footer-props="{
+        'items-per-page-options': [5, 10, 15, 20, 25, 30]
+      }"
+      class="elevation-1"
+    >
+      <template v-slot:item.pin="{ item }">
+        {{ item.pin != 0 ? "&#128204;" : "" }}
+      </template>
+      <template v-slot:item.views="{ item }">
+        {{ item.views }}
+      </template>
+      <template v-slot:item.linked="{ item }">
+        {{ item.linked }}
+      </template>
+      <template v-slot:item.updated="{ item }">
+        {{ formatDate(item.updated) }}
+      </template>
+      <template v-slot:item.title="{ item }">
+        <a
+        :href="'https://scrapbox.io/'+ projectName + '/' + encodeTitle(item.title)"
+        target="_blank">{{ item.title }}</a>    </template>
+      <template v-slot:item.image="{ item }">
+        <img :src="item.image" style="width: auto; height: 25px">
+      </template>
+    </v-data-table>
+  </v-card>
 </template>
 
 <script>
@@ -73,6 +87,7 @@
       },
     },
     data: () => ({
+      search: '',
       pages: [],
       types: ["pin", "updated", "title", "images"],
       itemsPerPage: 10,
