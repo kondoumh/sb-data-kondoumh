4a00a31537485e13ae58c168f9ad5976078ee216
kondoumh <kondoumh@gmail.com>
2020-06-10 19:00:07 +0900

    update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,47 +1,50 @@
 <template>
   <v-card
     class="mx-auto"
-    max-width="300"
-    tile
+    max-width="500"
   >
+    <v-list-item two-line>
+      <v-list-item-content>
+        <v-list-item-title class="headline mb-1">Scrapbox viz</v-list-item-title>
+        <v-list-item-subtitle>PWA for listing Scrapbox pages and graph visualization.</v-list-item-subtitle>
+      </v-list-item-content>
+      <img src="img/icons/apple-touch-icon-76x76.png" />
+    </v-list-item>
+
     <v-list shaped>
       <v-subheader>Powered by</v-subheader>
-      <v-list-item-group v-model="item" color="primary">
-        <v-list-item
-          v-for="(item, i) in itemsOss"
-          :key="i"
-          :href="item.url"
-          target="_blank"
-        >
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
+      <v-list-item
+        v-for="item in itemsOss"
+        :key="item.text"
+        :href="item.url"
+        target="_blank"
+      >
+        <v-list-item-icon>
+          <v-icon v-text="item.icon"></v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.text"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
     </v-list>
 
     <v-divider></v-divider>
 
     <v-list shaped>
       <v-subheader>Data from</v-subheader>
-      <v-list-item-group v-model="item" color="primary">
-        <v-list-item
-          v-for="(item, i) in itemsSite"
-          :key="i"
-          :href="item.url"
-          target="_blank"
-        >
-          <v-list-item-icon>
-            <v-icon v-text="item.icon"></v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
+      <v-list-item
+        v-for="item in itemsSite"
+        :key="item.text"
+        :href="item.url"
+        target="_blank"
+      >
+        <v-list-item-icon>
+          <v-icon v-text="item.icon"></v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.text"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
     </v-list>
   </v-card>
  </template>
@@ -49,7 +52,6 @@
 <script>
   export default {
     data: () => ({
-      item: 1,
       itemsOss: [
         { icon: 'mdi-flag', text: 'Vue.js', url: 'https://jp.vuejs.org' },
         { icon: 'mdi-flag', text: 'Vuetify', url: 'https://vuetifyjs.com' },
