7b5877b6c51a1a3e49da37e333f7e094ac3669e9
kondoumh <kondoumh@gmail.com>
2020-11-29 13:04:27 +0900

    add functions

---
+++ resources/api/sbpages.js
@@ -0,0 +1,28 @@
+exports.handler = async (e, c) => {
+
+  console.log(c)
+
+  const project = e.headers.project
+  if (!project) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({error: "Project name is required!"}),
+    };
+  }
+
+  const fetch = require("node-fetch").default;
+  const url = `https://scrapbox.io/api/pages/${project}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  if (res.status !== 200) {
+    return {
+      statusCode: res.status,
+      body: JSON.stringify(data),
+    }
+  }
+  console.log(data.projectName + " : " + data.count);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
