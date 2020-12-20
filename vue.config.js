c67c023ba341acfb66fdffe7e83b2793c9c216bc
kondoumh <kondoumh@gmail.com>
2020-12-20 12:56:57 +0900

    add pwa config

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,10 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     proxy: "https://sb-data-kondoumh.netlify.app"
+  },
+  pwa: {
+    workboxOptions: {
+      skipWaiting: true
+    }
   }
 }
